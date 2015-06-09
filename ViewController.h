b31a1931519527bee46a353b6506a264cfa7207c
jlemmons24 <jlemmons24@gmail.com>
2015-06-09 10:00:33 -0600

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts2
+//
+//  Created by Jordan Lemmons on 4/4/15.
+//  Copyright (c) 2015 Jordan Lemmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *funFactLabel;
+
+@end
