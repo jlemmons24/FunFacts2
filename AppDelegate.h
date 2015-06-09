15e0ca4a61be7b08a48e399e850828ef4b0dfc6a
jlemmons24 <jlemmons24@gmail.com>
2015-06-09 10:01:44 -0600

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  FunFacts2
+//
+//  Created by Jordan Lemmons on 4/4/15.
+//  Copyright (c) 2015 Jordan Lemmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
